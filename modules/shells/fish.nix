3624200824a349f3da176bc6e2395d273f799922
waifusslayer <125269978+waifusslayer@users.noreply.github.com>
2026-03-05 12:46:00 +0500

    Configure Fish shell to initialize Starship prompt

--- modules/shells/fish.nix
+++ modules/shells/fish.nix
@@ -2,8 +2,6 @@
 
 {
   programs.fish = {
-    # В fish completions подхватываются автоматически, если пакеты в PATH
-    # Если нужно — можно добавить interactiveShellInit
     interactiveShellInit = ''
       starship init fish | source
     '';
