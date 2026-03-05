d6bb18e37f7f7fc160851ffa0dc9f613a0ee0293
waifusslayer <125269978+waifusslayer@users.noreply.github.com>
2026-03-05 12:23:44 +0500

    Add Zsh configuration with Starship integration

---
+++ modules/shells/zsh.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, lib, ... }:
+
+{
+  programs.zsh = {
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    # Starship и completions только для zsh
+    initContent = lib.mkMerge [
+      (lib.mkOrder 500 ''
+        eval "$(starship init zsh)"
+      '')
+
+      (lib.mkOrder 550 ''
+        source <(kubectl completion zsh 2>/dev/null || true)
+        source <(helm completion zsh 2>/dev/null || true)
+        source <(kubectx completion zsh 2>/dev/null || true)
+      '')
+    ];
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      add_newline = false;
+      line_break.disabled = true;
+    };
+  };
+}
